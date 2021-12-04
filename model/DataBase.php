71e58bf5f4765fa62d278c740c403868f57df66a
SenaProfeAlbeiro <earamos42@misena.edu.co>
2021-12-04 14:56:38 -0500

    Prueba Github - Heroku - MySql

--- model/DataBase.php
+++ model/DataBase.php
@@ -3,7 +3,7 @@
 	class DataBase {
 
 		public static function conexion(){
-			$pdo = new PDO('mysql:host=localhost;dbname=bbdd_proyecto;charset=utf8', 'root', '');
+			$pdo = new PDO('mysql:host=br1yj1gpvqgd7aaftjxd-mysql.services.clever-cloud.com;dbname=br1yj1gpvqgd7aaftjxd;charset=utf8', 'uwup34oaojxkqt0f', 'iwmTNoBNidbmb1m9Ndvg');
         	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         	return $pdo;
 		}
